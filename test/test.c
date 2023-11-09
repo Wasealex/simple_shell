ee68187533b8ad4e2dbcf37f88064163f5432a6a
ethioendu <ethioendu21@gmail.com>
2023-11-09 13:12:01 +0300

    testing commit

--- test/test.c
+++ test/test.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: hello
+ *
+ */
 int main(void)
 {
-	printf("welcome");
+	char *command;
+	size_t clen;
+	getline(&command, &clen, stdin);
+	return (0);
 }
