3bc7f89390333e76483a88ddd366be2adb989c23
ethioendu <ethioendu21@gmail.com>
2023-11-09 13:19:58 +0300

    header file created

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+/*header files*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stddef.h>
+/*function prototype*/
+#endif /*SHELL_H*/
